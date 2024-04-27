ddd2a1d9b5dc0e12e1369aef7a11b4ecc7d94f8f
Даниил Ксенофонтов <daks@skbkontur.ru>
2024-04-27 12:42:03 +0500

    feat(CI): деплой на прод

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
-
 import vue from "@astrojs/vue";
+import env from '/src/utils/client'
+
+const { MODE } = env;
+const siteUrlMap = {
+  production: 'https://primego.online',
+  staging: 'https://daks01.github.io/lp_primego/',
+  development: 'http://localhost:4321/',
+};
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: siteUrlMap[MODE],
   integrations: [sitemap(), vue()]
 });
