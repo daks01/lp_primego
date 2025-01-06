a6a190dc811c889324036aa59e09f782d4f63a0a
Даниил Ксенофонтов <daks@skbkontur.ru>
2025-01-06 10:50:56 +0500

    feat(i18n): start

---
+++ src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+    en: 'En',
+    ru: 'Ru',
+};
+
+export const defaultLang = 'ru';
+
+export const showDefaultLang = false;
+
+export const ui = {
+    en: {
+        'nav.home': 'Home',
+        'nav.about': 'About',
+        'nav.twitter': 'Twitter',
+    },
+    ru: {
+        'nav.home': 'Accueil',
+        'nav.about': 'À propos',
+    },
+} as const;
